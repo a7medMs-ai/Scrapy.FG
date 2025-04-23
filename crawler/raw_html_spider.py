ce4f6dd8ea35e82068bc21b981c6adae460952b8
a7medMs-ai <ahmed.mostafsaad@gmail.com>
2025-04-24 00:58:42 +0200

    Update raw_html_spider.py

--- crawler/raw_html_spider.py
+++ crawler/raw_html_spider.py
@@ -11,7 +11,7 @@ class RawHtmlSpider(scrapy.Spider):
         "ROBOTSTXT_OBEY": True,
     }
 
-    def __init__(self, start_url=None, output_dir="downloaded_pages", *args, **kwargs):
+    def __init__(self, start_url=None, output_dir="html_pages", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.start_urls = [start_url]
         self.allowed_domains = [urlparse(start_url).netloc]
@@ -22,14 +22,13 @@ class RawHtmlSpider(scrapy.Spider):
         parsed_url = urlparse(response.url)
         path = parsed_url.path.strip("/").replace("/", "_") or "index"
         filename = f"{parsed_url.netloc}_{path}.html"
-
         full_path = os.path.join(self.output_dir, filename)
+
         with open(full_path, "wb") as f:
             f.write(response.body)
 
         yield {"url": response.url, "status": response.status, "saved_to": full_path}
 
-        # Follow only internal links
         for href in response.css("a::attr(href)").getall():
             absolute_url = response.urljoin(href)
             if self.should_follow(absolute_url):
